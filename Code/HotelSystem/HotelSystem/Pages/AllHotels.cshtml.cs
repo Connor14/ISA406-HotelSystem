67d4bb12f4a7472dda82b597fb8b1de39c8de5f3
Michael Gentile <gentilm5@miamioh.edu>
2019-04-23 14:30:03 -0400

    Added some sync with the Hotel page.

--- Code/HotelSystem/HotelSystem/Pages/AllHotels.cshtml.cs
+++ Code/HotelSystem/HotelSystem/Pages/AllHotels.cshtml.cs
@@ -3,32 +3,47 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using HotelSystem.Models;
+using Dapper;
 using HotelSystem.Application;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
 
 namespace HotelSystem.Pages
 {
     public class AllHotelsModel : PageModel
     {
-        public List<Models.Hotel> Hotels = new List<Models.Hotel>()
+        private IConfiguration _configuration;
+
+        public AllHotelsModel(IConfiguration configuration)
         {
-            new Models.Hotel() { HotelID = 1, Name = "Demo Name 1", State = "Ohio", City = "Oxford", Street = "5262 Brown Road", ZipCode = 45056 },
-            new Models.Hotel() { HotelID = 2, Name = "Demo Name 2", State = "Ohio", City = "Oxford", Street = "5262 Brown Road", ZipCode = 45056 },
-            new Models.Hotel() { HotelID = 3, Name = "Demo Name 3", State = "Ohio", City = "Oxford", Street = "5262 Brown Road", ZipCode = 45056 },
-            new Models.Hotel() { HotelID = 4, Name = "Demo Name 4", State = "Ohio", City = "Oxford", Street = "5262 Brown Road", ZipCode = 45056 },
-            new Models.Hotel() { HotelID = 5, Name = "Demo Name 5", State = "Ohio", City = "Oxford", Street = "5262 Brown Road", ZipCode = 45056 },
-            new Models.Hotel() { HotelID = 6, Name = "Demo Name 6", State = "Ohio", City = "Oxford", Street = "5262 Brown Road", ZipCode = 45056 },
-            new Models.Hotel() { HotelID = 7, Name = "Demo Name 7", State = "Ohio", City = "Oxford", Street = "5262 Brown Road", ZipCode = 45056 },
-            new Models.Hotel() { HotelID = 8, Name = "Demo Name 8", State = "Ohio", City = "Oxford", Street = "5262 Brown Road", ZipCode = 45056 },
-            new Models.Hotel() { HotelID = 9, Name = "Demo Name 9", State = "Ohio", City = "Oxford", Street = "5262 Brown Road", ZipCode = 45056 },
-            new Models.Hotel() { HotelID = 10, Name = "Demo Name 10", State = "Ohio", City = "Oxford", Street = "5262 Brown Road", ZipCode = 45056 }
-        };
+            this._configuration = configuration;
+        }
+
+        public List<Models.Hotel> Hotels = new List<Models.Hotel>();
+        //{
+        //    new Models.Hotel() { HotelID = 1, Name = "Demo Name 1", State = "Ohio", City = "Oxford", Street = "5262 Brown Road", ZipCode = 45056 },
+        //    new Models.Hotel() { HotelID = 2, Name = "Demo Name 2", State = "Ohio", City = "Oxford", Street = "5262 Brown Road", ZipCode = 45056 },
+        //    new Models.Hotel() { HotelID = 3, Name = "Demo Name 3", State = "Ohio", City = "Oxford", Street = "5262 Brown Road", ZipCode = 45056 },
+        //    new Models.Hotel() { HotelID = 4, Name = "Demo Name 4", State = "Ohio", City = "Oxford", Street = "5262 Brown Road", ZipCode = 45056 },
+        //    new Models.Hotel() { HotelID = 5, Name = "Demo Name 5", State = "Ohio", City = "Oxford", Street = "5262 Brown Road", ZipCode = 45056 },
+        //    new Models.Hotel() { HotelID = 6, Name = "Demo Name 6", State = "Ohio", City = "Oxford", Street = "5262 Brown Road", ZipCode = 45056 },
+        //    new Models.Hotel() { HotelID = 7, Name = "Demo Name 7", State = "Ohio", City = "Oxford", Street = "5262 Brown Road", ZipCode = 45056 },
+        //    new Models.Hotel() { HotelID = 8, Name = "Demo Name 8", State = "Ohio", City = "Oxford", Street = "5262 Brown Road", ZipCode = 45056 },
+        //    new Models.Hotel() { HotelID = 9, Name = "Demo Name 9", State = "Ohio", City = "Oxford", Street = "5262 Brown Road", ZipCode = 45056 },
+        //    new Models.Hotel() { HotelID = 10, Name = "Demo Name 10", State = "Ohio", City = "Oxford", Street = "5262 Brown Road", ZipCode = 45056 }
+        //};
 
         public void OnGet()
         {
-            // select * from hotels
+            using (var connection = new MySqlConnection(_configuration.GetConnectionString("Default")))
+            {
+                Hotels = connection.Query<Models.Hotel>("SELECT * FROM Hotel;").ToList<Models.Hotel>();
+            }
+
         }
 
         public IActionResult OnPostSelectHotel()
--- Code/HotelSystem/HotelSystem/Pages/Hotel/HotelMain.cshtml.cs
+++ Code/HotelSystem/HotelSystem/Pages/Hotel/HotelMain.cshtml.cs
@@ -3,21 +3,34 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Dapper;
 using HotelSystem.Application;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
 
 namespace HotelSystem.Pages.Hotel
 {
     public class HotelMainModel : PageModel
     {
+
+        private IConfiguration _configuration;
+
+        public HotelMainModel(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
         public Models.Hotel Hotel;
 
         public void OnGet()
         {
             int hotelId = HttpContext.Session.GetInt32(Constants.CurrentHotel).Value;
-            Hotel = new Models.Hotel() { HotelID = 1, Name = "Demo Name 1", State = "Ohio", City = "Oxford", Street = "5262 Brown Road", ZipCode = 45056 };
+            using (var connection = new MySqlConnection(_configuration.GetConnectionString("Default")))
+            {
+                Hotel = connection.QuerySingleOrDefault<Models.Hotel>("SELECT * FROM Hotel where HotelID = @HotelId;", new { HotelId = hotelId });
+            }
         }
     }
 }
