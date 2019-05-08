9923aff3a4b55a132494aedbe0957326f09a53c0
gentilm5 <gentilm5@miamioh.edu>
2019-05-07 23:14:38 -0400

    Loyalty

--- Code/HotelSystem/HotelSystem/Pages/Loyalty/Loyalty.cshtml.cs
+++ Code/HotelSystem/HotelSystem/Pages/Loyalty/Loyalty.cshtml.cs
@@ -1,16 +1,32 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Dapper;
+using HotelSystem.Application;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
 namespace HotelSystem.Pages.Loyalty
 {
     public class LoyaltyModel : PageModel
     {
+        private IConfiguration _configuration;
+        public int points;
+        public LoyaltyModel(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
         public void OnGet()
         {
+            int userId = HttpContext.Session.GetInt32(Constants.UserId).Value;
+            using (var connection = new MySqlConnection(_configuration.GetConnectionString("Default")))
+            {
+                points = connection.Query<int>("SELECT SUM(COST) FROM Reservation WHERE User_UserID = @userId;", new { userId }).First();
+            }
         }
     }
 }
